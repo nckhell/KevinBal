72aba52399ba1493983edf5cc1fba90d9b164b74
Nick <NickHellemans93@gmail.com>
2019-04-20 12:02:13 +0200

    testing

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import '../static/styles/index.css'
 
 export default () => (
     <div className="p-4 shadow rounded bg-white">
-      <h1 className="text-purple leading-normal">Next.js</h1>
+      <h1 className="text-purple leading-normal">About.js</h1>
       <p className="text-grey-dark">with Tailwind CSS</p>
     </div>
   )
