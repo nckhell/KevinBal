48b289f42f4c219573759c82cefe30a998bb2228
Nick <NickHellemans93@gmail.com>
2020-01-09 17:28:29 +0100

    Deploy to gh-pages

---
+++ _next/static/webpack/static/development/pages/visie.js.2112c7ddc4abcf17656d.hot-update.js
@@ -0,0 +1,101 @@
+webpackHotUpdate("static/development/pages/visie.js",{
+
+/***/ "./pages/visie.js":
+/*!************************!*\
+  !*** ./pages/visie.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_layouts_main__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/layouts/main */ "./components/layouts/main.js");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! next/head */ "./node_modules/next-server/dist/lib/head.js");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_2__);
+var _jsxFileName = "/Users/nick/Documents/Github/KevinBal/pages/visie.js";
+
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_layouts_main__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    activePath: "visie",
+    title: "Visie",
+    description: "Kinesitherapeut Kevin Bal geeft je graag wat meer inzicht over zijn visie en manier van werken",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_head__WEBPACK_IMPORTED_MODULE_2___default.a, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("link", {
+    rel: "canonical",
+    href: "http://kinebal.be/visie/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    className: "container m-auto flex flex-col py-10 max-w-xl text-center px-6",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {
+    htmlFor: "tagline",
+    className: "uppercase tracking-wide text-grey-dark font-bold",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, "Manier van werken"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    className: "my-4 font-normal",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    },
+    __self: this
+  }, "Visie"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("hr", {
+    className: "border-b w-8 m-auto my-6",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "text-justify text-lg leading-normal mb-4 text-grey-darkest mt-1 lg:mt-4 max-w-lg m-auto",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 22
+    },
+    __self: this
+  }, "Als echte Kontichnaar was het altijd zijn wens om in zijn eigen gemeente aan de slag te gaan als kinesitherapeut. Na 15 jaar ervaring te hebben opgedaan in zijn praktijk in Antwerpen en in het revalidatiecentrum APRA was de tijd rijp om ook in Kontich een praktijk op te starten."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "text-justify text-lg leading-normal mb-4 text-grey-darkest mt-1 lg:mt-4 max-w-lg m-auto",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 28
+    },
+    __self: this
+  }, "Zeker in deze setting wil hij de nadruk leggen op een individuele en manuele aanpak. Indien de aard van de aandoening dit vereist zal dit aangevuld worden met actieve oefentherapie afgestemd op pati\xEBnt en pathologie. De samenwerking tussen pati\xEBnt en therapeut staat hierbij uiteraard centraal. Door middel van zachte mobilisaties, harmonieuze technieken en gerichte manipulaties wordt er getracht de beperkte mobiliteit van gewrichten te vergroten waarna gerichte oefentherapie kan opgestart worden om zo het weefselherstel te bevorderen. Belangrijk hierbij is het evenwicht tussen belasting en belastbaarheid te bewaren en de pati\xEBnt functioneel, progressief en doelgericht op te trainen. De pati\xEBnt van zijn kant draagt in zijn herstel zeker ook een verantwoordelijkheid. Alle oefeningen en adviezen voor thuis zullen dan ook nageleefd moeten worden om het gezamenlijk doel te bereiken.", " "), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "text-justify text-lg leading-normal mb-4 text-grey-darkest mt-1 lg:mt-4 max-w-lg m-auto",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  }, "Alle behandelingen gebeuren na afspraak en omvatten een gemiddelde duur van 30 minuten. Hij doet steeds zijn uiterste best om u niet te lang te laten wachten en voor iedereen de nodige tijd te voorzien. Het wordt dan ook sterk geapprecieerd dat u zich aan de gemaakte afspraken houdt. Buiten uw wil om kunnen er zich soms omstandigheden voordoen dewelke u niet in staat stellen de afspraak na te komen. Er wordt u gevraagd om bij belet 12u op voorhand te verwittigen om eventueel een andere pati\xEBnt te kunnen verzorgen. Mocht u niet in de mogelijkheid zijn tijdig te verwittigen of indien u een afspraak vergeet, dan wordt er verwacht dat u zo sportief bent om de voor u vrij gehouden tijd te vergoeden.")));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=visie.js.2112c7ddc4abcf17656d.hot-update.js.map
